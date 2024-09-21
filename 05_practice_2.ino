4287dc8aa0034d4743c8bfb0cd997d1a1579199f
baekyeomin <116693377+baekyeomin@users.noreply.github.com>
2024-09-22 03:36:50 +0900

    05_practice_2

---
+++ 05_practice_2.ino
@@ -0,0 +1,27 @@
+#define PIN_LED 7
+
+void setup(){
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  // 1초 동안 LED 켜기
+  digitalWrite(PIN_LED, 0);
+
+  delay(1000);
+
+  //1초 동안 LED 5번 깜빡이기
+  for (int i = 0; i<5; i++){
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+    digitalWrite(PIN_LED, 0);
+    delay(100);
+  }
+  
+  //LED 끄고 무한루프 
+  digitalWrite(PIN_LED, 1);
+  while(1){
+    //아무 동작도 하지 않는 대기 상태
+  }
+}
+
