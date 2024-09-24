58f590782aabf1c47744c3af79522d5f0fcca289
baekyeomin <116693377+baekyeomin@users.noreply.github.com>
2024-09-24 11:13:21 +0900

    Update 05_practice_2.ino

--- 05_practice_2.ino
+++ 05_practice_2.ino
@@ -6,20 +6,20 @@ void setup(){
 
 void loop() {
   // 1초 동안 LED 켜기
-  digitalWrite(PIN_LED, 0);
+  digitalWrite(PIN_LED, 1);
 
   delay(1000);
 
   //1초 동안 LED 5번 깜빡이기
   for (int i = 0; i<5; i++){
-    digitalWrite(PIN_LED, 1);
-    delay(100);
     digitalWrite(PIN_LED, 0);
+    delay(100);
+    digitalWrite(PIN_LED, 1);
     delay(100);
   }
   
   //LED 끄고 무한루프 
-  digitalWrite(PIN_LED, 1);
+  digitalWrite(PIN_LED, 0);
   while(1){
     //아무 동작도 하지 않는 대기 상태
   }
